c299a85395555da27dde70db4d96dda1c93195c8
Raz <raz-@outlook.com>
2019-06-20 23:29:37 -0700

    start

---
+++ src/sync/mod.rs
@@ -0,0 +1,10 @@
+//! # Sync
+//!
+//! `sync` contains functions and data structures used to synchronize multithreaded applications.
+//!
+
+/// Contains structures and functionality to use a critical section.
+pub mod critical_section;
+
+/// Contains functionality to atomically access and modify data.
+pub mod atomic;
