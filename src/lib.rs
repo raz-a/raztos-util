6d0525c18ef771129d8ee8499ca2fd60042114cf
Raz <raz-@outlook.com>
2019-06-25 22:54:42 -0700

    Describe

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
-// RAZTODO Document what raztos_util is
+//! # RazTOS Utilities
+//!
+//! `raztos_util` is a stand-alone, no-std crate that contains useful utilities for embedded
+//! systems applications.
+//!
+//! These utilities include:
+//! * Search and Sort Algorithms with low jitter.
+//!
+//! * Dynamic Memory Allocators with controllable sizes and constant time
+//!   (or near constant time) allocations/frees.
+//!
+//! * Common Item collections for efficiently storing structures with low memory footprint.
+//!
+//! * Compile-time CPU feature checks and instruction-set optimized procedures.
+//!
+//! * Synchronization primitives for multi-threaded applications.
+//!
+//! This crate is used by RazTOS as a foundational library.
 
 #![cfg_attr(not(test), no_std)]
 
 pub mod algorithms;
 
+/// Memory Allocators with real-time guarantees.
 pub mod allocators;
 
 pub mod collections;
