4e10817b5513b53c52bb1bc7d87a363778507c6c
Raz <raz-@outlook.com>
2019-07-20 20:36:10 -0700

    Tests

--- src/allocators/monotonic_allocator.rs
+++ src/allocators/monotonic_allocator.rs
@@ -53,7 +53,7 @@ impl<'a> MonotonicAllocatorInternal<'a> {
         if layout.size() != 0 {
             if (self.heap.len() - aligned_index) >= layout.size() {
                 let out_ptr = self.heap.get_unchecked_mut(aligned_index);
-                self.free_index = aligned_index;
+                self.free_index = aligned_index + layout.size();
                 return out_ptr;
             }
         }
@@ -216,8 +216,96 @@ mod tests {
             assert_eq!(internal.free_index, original_free_index);
 
             let global_alloc_result = GlobalAlloc::alloc(&allocator, zero_sized);
-            assert_eq!(global_alloc_result,core::ptr::null_mut());
+            assert_eq!(global_alloc_result, core::ptr::null_mut());
             assert_eq!(internal.free_index, original_free_index);
+        }
+    }
+
+    #[test]
+    fn oversized_allocation_returns_null() {
+        let mut backing_memory = AlignedBackingMemory([0; SIZE_4K]);
+        let allocator = MonotonicAllocator::new(&mut backing_memory.0[..]);
+        let mut allocator = allocator.unwrap();
+
+        let size = SIZE_4K * 2;
+
+        unsafe {
+            let over_sized = Layout::from_size_align_unchecked(size, size);
+            let internal = &mut *allocator.0.get();
+            let original_free_index = internal.free_index;
+
+            let alloc_result = Alloc::alloc(&mut allocator, over_sized);
+            assert!(alloc_result.is_err());
+            assert_eq!(internal.free_index, original_free_index);
+
+            let global_alloc_result = GlobalAlloc::alloc(&allocator, over_sized);
+            assert_eq!(global_alloc_result, core::ptr::null_mut());
+            assert_eq!(internal.free_index, original_free_index);
+        }
+    }
+
+    #[test]
+    fn valid_allocation_progresses_allocator() {
+        let mut backing_memory = AlignedBackingMemory([0; SIZE_4K]);
+        let allocator = MonotonicAllocator::new(&mut backing_memory.0[..]);
+        let mut allocator = allocator.unwrap();
+
+        unsafe {
+            let valid = Layout::from_size_align_unchecked(16,16);
+            let internal = &mut *allocator.0.get();
+            let original_free_index = internal.free_index;
+
+            let alloc_result = Alloc::alloc(&mut allocator, valid);
+            assert!(alloc_result.is_ok());
+            assert!(internal.free_index > original_free_index);
+
+            let original_free_index = internal.free_index;
+
+            let global_alloc_result = GlobalAlloc::alloc(&allocator, valid);
+            assert_ne!(global_alloc_result, core::ptr::null_mut());
+            assert!(internal.free_index > original_free_index);
+        }
+    }
+
+    #[test]
+    fn allocations_are_unique() {
+        let mut backing_memory = AlignedBackingMemory([0; SIZE_4K]);
+        let allocator = MonotonicAllocator::new(&mut backing_memory.0[..]);
+        let mut allocator = allocator.unwrap();
+
+        unsafe {
+            let valid = Layout::from_size_align_unchecked(16,16);
+
+            let alloc_result = Alloc::alloc(&mut allocator, valid).unwrap();
+            let alloc_result2 = Alloc::alloc(&mut allocator, valid).unwrap();
+            assert_ne!(alloc_result, alloc_result2);
+
+            let global_alloc_result = GlobalAlloc::alloc(&allocator, valid);
+            let global_alloc_result2 = GlobalAlloc::alloc(&allocator, valid);
+
+            assert_ne!(global_alloc_result, global_alloc_result2);
+        }
+    }
+
+    #[test]
+    fn free_does_nothing() {
+        let mut backing_memory = AlignedBackingMemory([0; SIZE_4K]);
+        let allocator = MonotonicAllocator::new(&mut backing_memory.0[..]);
+        let mut allocator = allocator.unwrap();
+
+        unsafe {
+            let valid = Layout::from_size_align_unchecked(16,16);
+            let internal = &mut *allocator.0.get();
+
+            let alloc_result = Alloc::alloc(&mut allocator, valid).unwrap();
+            let current_free_index = internal.free_index;
+            Alloc::dealloc(&mut allocator, alloc_result, valid);
+            assert_eq!(internal.free_index, current_free_index);
+
+            let global_alloc_result = GlobalAlloc::alloc(&allocator, valid);
+            let current_free_index = internal.free_index;
+            GlobalAlloc::dealloc(&allocator, global_alloc_result, valid);
+            assert_eq!(internal.free_index, current_free_index);
         }
     }
 }
